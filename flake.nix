fe5dec29a9c9a3b9193b8da30cfeea6a37681942
A0hijlxs <achilles010203@gmail.com>
2026-09-21 12:40:52 +0100

    Add reference corpora and Nix devShell
    Sample English/French texts for n-gram frequency modelling and word-pattern
    dictionary attacks, plus a Jupyter/pytest devShell for the project.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+    description = "annealing-cryptanalysis devshell with Jupyter";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+                python = pkgs.python3.withPackages (ps: with ps; [
+                    jupyterlab
+                    numpy
+                    pandas
+                    matplotlib
+                    pycryptodome
+                    sympy
+                    requests
+                    pytest
+                ]);
+            in
+                {
+                devShells.default = pkgs.mkShell {
+                    packages = [ python ];
+
+                    shellHook = ''
+            export PYTHONPATH="$PWD/src:$PYTHONPATH"
+            echo "codebreak package is on PYTHONPATH (src/)."
+            echo "Run 'pytest' to run the tests, or 'jupyter lab' to start the notebook server."
+                    '';
+                };
+            });
+}
